1c8480c7861d9e75d758b032e18cbaccfec97aab
Duc Nguyen <ducgaogao4213@gmail.com>
2019-08-08 11:47:31 +0700

    update api endpoints

--- app/api/Endpoints.js
+++ app/api/Endpoints.js
@@ -1,2 +1,6 @@
 const BASE = "http://ec2-54-251-181-28.ap-southeast-1.compute.amazonaws.com:8080/golfer_api/api/"
 export const LOGIN = BASE + "login"
+export const GET_COUNTRY = BASE + "country"
+export const GET_REGION = BASE + "region/{countryId}"
+export const GET_CLUB = BASE + "club/{regionId}"
+export const GET_FRIEND_LIST = BASE + "authenticated/friend/{userId}"
